f09871784391cc90dbd1c432203c9aa8ade12c81
Christiana <christianaramey@gmail.com>
2021-04-10 02:11:27 -0500

    added invoker URL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'eyJraWQiOiJNd3hocUNTcFJJcUc1SmZzcFlkeDlyM1poOUc2NFF5VGg0S0ZEQ1wvSTcyYz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzZGJlNzA3Mi1lZDZiLTQxZmYtYmQ1MS03MDk0YTVhYWFmMjMiLCJhdWQiOiI5N3FzamJudTVkbnMyN2VoY2xxc2xuNTloIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV2ZW50X2lkIjoiZTJhYzg2NzUtMDI0Ny00ODc5LWE1MjItYmFlMThmMzM5YjE4IiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2MTgwMjA5MjYsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy13ZXN0LTIuYW1hem9uYXdzLmNvbVwvdXMtd2VzdC0yX0U4Tmx5ZTFEWCIsImNvZ25pdG86dXNlcm5hbWUiOiJjaHJpc3RpYW5hcmFtZXktYXQtZ21haWwuY29tIiwiZXhwIjoxNjE4MDQxNDQwLCJpYXQiOjE2MTgwMzc4NDAsImVtYWlsIjoiY2hyaXN0aWFuYXJhbWV5QGdtYWlsLmNvbSJ9.DxSeJnSdsA4EzLst2XyNOay5KCVuHdvl1qJMm4Zsnzodt5D20Sn3aPxhiC0YjBE8vqORk-RsWa-scPwMTVH0tEvQAgSqeR_SWBiceAaloJeUPMaRwFuVupOlFy_oDflDRQwaZlgubm1h2Wftmuq45S7Nwkvss5yQzoELOVO30d6hf8svhQjTxN6boBrvQp8yAnCp-0hwBl_HhbEWhFlt7SeIyL-MvuutIGuH12glwiqzjoBl15oR18UD8L1xsK2kOWkPBMGVlHZNlES09v28rzYUnSn6IVCYNBCzM6Licnd0sYx0Ku1gceuI_H4N2rlzdwUoSR7MjUk4JhfIIoMZ3w' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://xt9vq3t1nl.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
